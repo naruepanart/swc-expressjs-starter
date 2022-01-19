47b7cae33a1f541bdf56092189128bb7147b6a3f
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2022-01-19 20:54:54 +0700

    Initial commit

---
+++ src/routes/api.route.ts
@@ -0,0 +1,9 @@
+/* const router = require('express').Router(); */
+import express from 'express';
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  res.send({ message: 'Ok api is working 🚀' });
+});
+
+export default router
